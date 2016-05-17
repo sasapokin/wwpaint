7ec3a94202a4489849595e8199e03e88aa506e19
Sasapokin <sasapokin@gmail.com>
2016-05-17 19:07:44 +0300

    Set up strict Lint options

--- jakefile.js
+++ jakefile.js
@@ -1,7 +1,36 @@
-task("default", ["lint"]);
+/*global desc, task, jake, fail, complete */
+(function(){
+    "use strict";
+    function nodeLintOptions(){
+        return {
+            bitwise: true,
+            curly: false,
+            eqeqeq: true,
+            forin: true,
+            immed: true,
+            latedef: true,
+            newcap: true,
+            noarg: true,
+            noempty: true,
+            nonew: true,
+            regexp: true,
+            undef: true,
+            strict: true,
+            trailing: true,
+            node: true
+        };
+    }
+    task("default", ["lint"]);
 
-desc("Lint everything");
-task("lint", [], function(){
-    var lint = require("./build/lint/lint_runner.js");
-    lint.validateFile("jakefile.js", {}, {});
-});
+    desc("Lint everything");
+    task("lint", [], function(){
+        var lint = require("./build/lint/lint_runner.js");
+
+        var files = new jake.FileList();
+        files.include("**/*.js");
+        files.exclude("node_modules");
+        lint.validateFileList(files.toArray(), nodeLintOptions(), {});
+    });
+
+
+}());
