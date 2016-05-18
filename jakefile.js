57124cc64253bbfb8796e3432bff99eeee1c5423
Sasapokin <sasapokin@gmail.com>
2016-05-18 10:09:24 +0300

    Updated to-do list; continuous integration is finished

--- jakefile.js
+++ jakefile.js
@@ -3,6 +3,7 @@
 (function(){
     "use strict";
 
+    desc("Build and Test");
     task("default", ["lint"]);
 
     desc("Lint everything");
@@ -13,7 +14,21 @@
         files.include("**/*.js");
         files.exclude("node_modules");
         var options = nodeLintOptions();
-        lint.validateFileList(files.toArray(), options, {});
+        var passed = lint.validateFileList(files.toArray(), options, {});
+        if(!passed) fail("Lint failed");
+    });
+
+    desc("Integrate");
+    task("integrate", ["default"], function(){
+        console.log("1. Make sure 'git status' is clean.");
+        console.log("2. Build on the integration box.");
+        console.log("   a. Walk over to integration box.");
+        console.log("   b. 'git pull'");
+        console.log("   c. 'jake'");
+        console.log("   d. If jake fails, stop! Try again after fixing the issue.");
+        console.log("3. 'git checkout integration'");
+        console.log("4. 'git merge master --no-ff --log'");
+        console.log("5. 'git checkout master'");
     });
 
     function nodeLintOptions(){
